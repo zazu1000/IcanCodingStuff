b1e436545899481ca729a7cb217aa978e67a4002
zazu1000 <160942917+zazu1000@users.noreply.github.com>
2024-03-20 19:56:15 -0400

    Add files via upload

--- worksheet.py
+++ worksheet.py
@@ -1,4 +1,5 @@
 #1
+
 # num = int(input("Input a number: "))
 # exp = int(input("Input an exponent: "))
 # number = 1
@@ -10,6 +11,7 @@
 
 
 #2
+
 # num_list = [1,2,3,4,5]
 # x = 1
 
@@ -18,17 +20,61 @@
 #     x = x * 10 + 1
 
 #3
-def num_line():
-    first_num = int(input("First number: "))
-    second_num = int(input("Second number: "))
 
-    times = second_num - first_num + 1
+# def num_line():
+#     first_num = int(input("First number: "))
+#     second_num = int(input("Second number: "))
 
-    if first_num > second_num:
-        for i in range(times):
-            print(first_num)
-            first_num = first_num + 1
-    
-    
+#     times = second_num - first_num + 1
+#     times2 = first_num - second_num + 1
 
-num_line()
+#     if first_num > second_num:
+#         for i in range(times2):
+#             print(second_num)
+#             second_num = second_num + 1
+#     else:
+#         for i in range(times):
+#             print(first_num)
+#             first_num = first_num + 1
+# num_line()
+
+#4
+
+# def prime_num():
+#     first_num = int(input("First number: "))
+#     if first_num < 2:
+#         first_num = 2
+#     second_num = int(input("Second number: "))
+#     prime = 0
+#     prime_nums = []
+#     for num in range(first_num, second_num + 1):
+#         if num > 3:
+#             prime = 0
+#             for i in range(2, num):
+#                 if num % i == 0:
+#                     break
+#                 else:                 
+#                     prime = prime + 1
+#                     if prime > num / 2:   
+#                         prime_nums.append(num)
+#                         break
+                    
+#         else:
+#             prime_nums.append(num)
+
+#     print(prime_nums)
+# prime_num()
+
+#5
+
+# def factorial():
+#     number = int(input("Number: "))
+#     final = 1
+
+#     for i in range(number + 1):
+#         if i > 1:
+#             final = i * final
+       
+#     print(f"{number}! is {final}")
+
+# factorial()
